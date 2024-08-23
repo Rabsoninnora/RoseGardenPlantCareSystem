06d44a20b4429d2c63e02e6768a2d559f389a31a
Rabson Innocent Phiri <rabsoninnora@gmail.com>
2024-08-23 14:44:04 +0200

    modified the main ui and DB

--- mainwindowplantcare.cpp
+++ mainwindowplantcare.cpp
@@ -1,7 +1,9 @@
 #include "mainwindowplantcare.h"
+#include "databaseheader.h"
 #include "./ui_mainwindowplantcare.h"
 #include <QMessageBox>
 #include<QLineEdit>
+#include<QSqlQuery>
 
 
 MainWindowPlantCare::MainWindowPlantCare(QWidget *parent)
@@ -11,11 +13,6 @@ MainWindowPlantCare::MainWindowPlantCare(QWidget *parent)
     ui->setupUi(this);
     //the cancel button on login ui
     //connect(ui->pushButton_login_3, SIGNAL(clicked()),this, SLOT(close()));
-
-
-
-
-
 
 
 
@@ -43,11 +40,38 @@ void MainWindowPlantCare::on_pushButton_login_clicked() {
         secDialog->show();
 
 
-    }
-       else
-         {
-         QMessageBox::warning(this, "Sorry","Username or password is not correct!");
-    }
+    } else{
+        db.open();
+        QSqlDatabase::database().transaction();
+        QSqlQuery Query_login(db);
+        Query_login.prepare("SELECT * FROM Adminlogin WHERE UserName='" +username+ "' AND PassWord='"+ password +"'");
+
+      int   FindCount=0;
+        if(Query_login.exec()){
+            while (Query_login.next()) {
+
+                FindCount= FindCount +1;//if UserName and PassWord is correct
+            }
+            if(FindCount==1) //if UserName and PassWord is correct
+            {
+            QMessageBox::information(this, "Welcome","Login is successful!");
+                //hide first window
+                // hide();
+                //creating a constructor for System Dashboard
+                secDialog =new SecDialog(this);
+                //calling secDialog object
+                secDialog->show();
+
+            } else if(FindCount==0) //if UserName and PassWord is not correct
+            {
+             QMessageBox::warning(this, "Sorry","Username or password is not correct!");
+            }
+        }
+
+        QSqlDatabase::database().commit();
+        db.close();
+      }
+
     // Clear the login form after form submition
     foreach(QLineEdit *widget, this->findChildren<QLineEdit*>()){widget->clear();
 
@@ -62,7 +86,7 @@ void MainWindowPlantCare::on_pushButton_login_clicked() {
 void MainWindowPlantCare::on_pushButton_login_3_clicked()
 {
     QMessageBox::StandardButton reply;
-    reply=QMessageBox::question(this,"Warning!!", "Close the Application?",QMessageBox::Yes | QMessageBox::No);
+    reply=QMessageBox::warning(this,"Warning!!", "Close the Application?",QMessageBox::Yes | QMessageBox::No);
     if(reply==QMessageBox::Yes)
     {
         QApplication::quit();
